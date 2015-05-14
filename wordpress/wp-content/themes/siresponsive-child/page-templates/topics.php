6ee57e677b509cbabb718db6c2513e4527ab6d61
dajia <dajia.dim@gmail.com>
2015-05-14 12:07:19 +0300

    display body at topics.php

--- wordpress/wp-content/themes/siresponsive-child/page-templates/topics.php
+++ wordpress/wp-content/themes/siresponsive-child/page-templates/topics.php
@@ -10,32 +10,28 @@ Template Name: Topics
 
 
 
-
-<?php while ( have_posts() ) : the_post(); ?>
-
-
-
-  <div class="row page" style="margin-bottom: 20px;">
-    <div class="twelve columns">
-      <?php the_title( '<h1 class="twelve columns page_heading">', '</h1>' ); ?>
-      <ul class="sidebar-menu" style="text-align: center;">
-        <?php
-        $parentID = ICL_LANGUAGE_CODE == get_the_ID();
-        $pages    = get_pages( array(
-          'post_type' => 'page',
-          'parent'    => get_the_ID(),
-          'sort_column' => 'menu_order'
-        ) );
-        ?>
-        <?php foreach ( $pages as $page ): ?>
-          <li>
-            <a <?php echo $post->ID==$page->ID; ?>
-              href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a>
-          </li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
+<div class="row page" style="margin-bottom: 20px;">
+  <div class="twelve columns">
+    <?php the_title( '<h1 class="twelve columns page_heading">', '</h1>' ); ?>
+    <ul class="sidebar-menu" style="text-align: center;">
+      <?php
+      $parentID = ICL_LANGUAGE_CODE == get_the_ID();
+      $pages    = get_pages( array(
+        'post_type' => 'page',
+        'parent'    => get_the_ID(),
+        'sort_column' => 'menu_order'
+      ) );
+      ?>
+      <?php foreach ( $pages as $page ): ?>
+        <li>
+          <a <?php echo $post->ID==$page->ID; ?>
+            href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
   </div>
+</div>
+<?php while ( have_posts() ) : the_post(); ?>
 
 <div class="row page" style="margin-bottom: 20px;">
   <div class="twelve columns">
