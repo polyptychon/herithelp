31da2717299996a0754fa4677ce7b8bb3ac57fa3
dajia <dajia.dim@gmail.com>
2015-05-14 12:04:50 +0300

    display body at topics.php

--- wordpress/wp-content/themes/siresponsive-child/page-templates/topics.php
+++ wordpress/wp-content/themes/siresponsive-child/page-templates/topics.php
@@ -39,7 +39,7 @@ Template Name: Topics
 
       <?php while ( have_posts() ) : the_post(); ?>
 
-        <?php get_template_part( 'content', 'page' ); ?>
+        <?php the_content(); ?>
 
       <?php endwhile; // end of the loop. ?>
 
