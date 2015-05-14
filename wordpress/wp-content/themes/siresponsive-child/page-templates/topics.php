44604f1d9ddd5c399fc5a4230652170795ab175e
dajia <dajia.dim@gmail.com>
2015-05-14 10:47:33 +0300

    display body at topics.php

--- wordpress/wp-content/themes/siresponsive-child/page-templates/topics.php
+++ wordpress/wp-content/themes/siresponsive-child/page-templates/topics.php
@@ -12,9 +12,13 @@ Template Name: Topics
 
 
 <?php while ( have_posts() ) : the_post(); ?>
+
+
+
   <div class="row page" style="margin-bottom: 20px;">
     <div class="twelve columns">
       <?php the_title( '<h1 class="twelve columns page_heading">', '</h1>' ); ?>
+      <?php the_content(); ?>
       <ul class="sidebar-menu" style="text-align: center;">
         <?php
         $parentID = ICL_LANGUAGE_CODE == get_the_ID();
